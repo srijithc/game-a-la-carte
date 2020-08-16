e881fc83b353a59358087ebcfd7137a258cf5b70
srijith <csrijith@smartzip.com>
2020-08-16 18:37:48 +0530

    Final cleanup

--- test/controllers/user_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UserControllerTest < ActionDispatch::IntegrationTest
-  test "should get signout" do
-    get user_signout_url
-    assert_response :success
-  end
-
-end
